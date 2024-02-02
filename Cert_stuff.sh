ba1e44d51f5ee3155d384bd6457bd6f5e1d5b34d
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2024-02-02 17:12:21 +0100

    Update Cert_stuff.sh

--- Cert_stuff.sh
+++ Cert_stuff.sh
@@ -1,5 +1,7 @@
 #?/bin/bash
 
+#AI generated for testing do not submit as it is
+
 kubectl create namespace cert-manager
 
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.13.1/cert-manager.yaml
