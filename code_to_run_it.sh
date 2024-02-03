26036b82893008b764fb11b14130b0994da51a5c
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2024-02-03 15:04:19 +0100

    Create code_to_run_it.sh

---
+++ code_to_run_it.sh
@@ -0,0 +1,18 @@
+#?/bin/bash
+
+#build the two images
+docker build -t api-service:latest ./api
+docker build -t ui-service:latest ./frontend
+
+#enable registry
+microk8s enable registry
+
+#tag the images for registry
+docker tag api-service:latest localhost:32000/api-service:latest
+docker tag ui-service:latest localhost:32000/ui-service:latest
+
+#push the images to registry
+docker push localhost:32000/api-service:latest
+docker push localhost:32000/ui-service:latest
+
+#helm install <release_name> ./k8s/
